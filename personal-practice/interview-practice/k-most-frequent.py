42e2b76048db5514380507f429480a12b2fe42a3
krishna19-me <harikrishnachn@gmail.com>
2024-07-22 11:13:40 +0530

    before autodesk interview

--- personal-practice/interview-practice/k-most-frequent.py
+++ personal-practice/interview-practice/k-most-frequent.py
@@ -1,5 +1,5 @@
 array = [1, 2, 2, 3, 3, 4]
-
+from collections import Counter
 
 def freq(array, k):
     arrayset = set(array)
@@ -21,6 +21,10 @@ def method2(array, k):
             dict1[i] = 1
     return [num for num, count in dict1.items() if count >= k]
 
+def method3(array, k):
+    counters = Counter(array)
+    return [count for count in counters if counters[count]>=k]
 
 print(freq(array, 2))
 print(method2(array, 2))
+print(method3(array, 2))
