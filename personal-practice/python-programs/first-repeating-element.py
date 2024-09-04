3fa2b589e272a54ae693967e4cb0bfe41323b5cd
krishna19-me <harikrishnachn@gmail.com>
2024-09-04 19:10:05 +0530

    binary, first-repeating-element

--- personal-practice/python-programs/first-repeating-element.py
+++ personal-practice/python-programs/first-repeating-element.py
@@ -8,3 +8,15 @@ def find_repeating(lst):
     return -1
 
 print(find_repeating(list1))
+
+def find_repeating_1(lst):
+    set = dict()
+    idx = -1
+    for i in range(len(lst)):
+        if lst[i] in set:
+            idx = i
+        else:
+            set[lst[i]] =1
+    return f"Element is {lst[idx]}"
+
+print(find_repeating_1(list1))
